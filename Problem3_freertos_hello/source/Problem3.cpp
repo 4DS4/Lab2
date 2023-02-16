c6c8a79346f4219cbcb52fc10253296a6a0aaa29
aronm99 <markanda@mcmaster.ca>
2023-02-16 12:51:05 -0500

    Finish problem 1-3

---
+++ Problem3_freertos_hello/source/Problem3.cpp
@@ -0,0 +1,18 @@
+/*
+ * Problem3.cpp
+ *
+ *  Created on: Feb. 15, 2023
+ *      Author: markanda
+ */
+
+#include "Problem3.h"
+
+Problem3::Problem3() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Problem3::~Problem3() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Problem3_freertos_hello/source/Problem3.h
@@ -0,0 +1,121 @@
+///*
+// * Copyright (c) 2015, Freescale Semiconductor, Inc.
+// * Copyright 2016-2017 NXP
+// * All rights reserved.
+// *
+// * SPDX-License-Identifier: BSD-3-Clause
+// */
+//
+///* FreeRTOS kernel includes. */
+//#include "FreeRTOS.h"
+//#include "task.h"
+//#include "queue.h"
+//#include "timers.h"
+//#include "semphr.h"
+//
+///* Freescale includes. */
+//#include "fsl_device_registers.h"
+//#include "fsl_debug_console.h"
+//#include "pin_mux.h"
+//#include "clock_config.h"
+//#include "board.h"
+//
+///*******************************************************************************
+// * Definitions
+// ******************************************************************************/
+//
+///* Task priorities. */
+///*******************************************************************************
+// * Prototypes
+// ******************************************************************************/
+//
+///*******************************************************************************
+// * Code
+// ******************************************************************************/
+///*!
+// * @brief Application entry point.
+// */
+//int counter = 0;
+//char str[20] = "";
+//
+//void producer_sem(void* pvParameters)
+//{
+//	SemaphoreHandle_t* semaphore = (SemaphoreHandle_t*)pvParameters;
+//	BaseType_t status1, status2;
+//	while(1)
+//	{
+//		PRINTF("Enter value: ");
+//		scanf("%s", &str);
+//		xSemaphoreGive(semaphore);
+//		xSemaphoreGive(semaphore);
+//		vTaskDelay(1000 / portTICK_PERIOD_MS);
+//	}
+//}
+//
+//void consumer1_sem(void* pvParameters)
+//{
+//	SemaphoreHandle_t* semaphore = (SemaphoreHandle_t*)pvParameters;
+//	BaseType_t status;
+//	while(1)
+//	{
+//		status = xSemaphoreTake(semaphore, portMAX_DELAY);
+//		if (status != pdPASS)
+//		{
+//			PRINTF("Failed to acquire producer1_semaphore\r\n");
+//			while (1);
+//		}
+//		PRINTF("%s\r\n", &str);
+//	}
+//}
+//
+//void consumer2_sem(void* pvParameters)
+//{
+//	SemaphoreHandle_t* semaphores = (SemaphoreHandle_t*)pvParameters;
+//	SemaphoreHandle_t producer2_semaphore = semaphores[1];
+//	SemaphoreHandle_t consumer_semaphore = semaphores[2];
+//	BaseType_t status;
+//	while(1)
+//	{
+//		xSemaphoreGive(consumer_semaphore);
+//		status = xSemaphoreTake(producer2_semaphore, portMAX_DELAY);
+//		if (status != pdPASS)
+//		{
+//			PRINTF("Failed to acquire producer2_semaphore\r\n");
+//			while (1);
+//		}
+//		PRINTF("Received Value = %d\r\n", counter);
+//	}
+//}
+//
+//int main(void)
+//{
+//	BaseType_t status;
+//	/* Init board hardware. */
+//	BOARD_InitBootPins();
+//	BOARD_InitBootClocks();
+//	BOARD_InitDebugConsole();
+//	SemaphoreHandle_t* semaphores = xSemaphoreCreateCounting(2, 2); //Producer_sem
+//
+//	status = xTaskCreate(producer_sem, "producer", 200, (void*)semaphores, 2, NULL);
+//	if (status != pdPASS)
+//	{
+//		PRINTF("Task creation failed!.\r\n");
+//		while (1);
+//	}
+//	status = xTaskCreate(consumer1_sem, "consumer", 200, (void*)semaphores, 2, NULL);
+//	if (status != pdPASS)
+//	{
+//		PRINTF("Task creation failed!.\r\n");
+//		while (1);
+//	}
+//	status = xTaskCreate(consumer2_sem, "consumer", 200, (void*)semaphores, 2, NULL);
+//	if (status != pdPASS)
+//	{
+//		PRINTF("Task creation failed!.\r\n");
+//		while (1);
+//	}
+//	vTaskStartScheduler();
+//	while (1)
+//	{}
+//}
+//
